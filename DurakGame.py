d61057e98e0e3b260f68586e79a1736f20b819cd
Emily H <115581555+ehelm2@users.noreply.github.com>
2022-12-19 21:21:08 -0500

    Update DurakGame.py

--- DurakGame.py
+++ DurakGame.py
@@ -9,6 +9,7 @@ from card_dictionary import card_files
 from random import shuffle
 from time import sleep
 
+
 class Card:
     """
     A card object.
@@ -48,7 +49,6 @@ class Card:
             Otherwise, False is returned.
 
         """
-        
         if self.value < card2.value:
             return True
         return False
@@ -70,7 +70,6 @@ class Card:
             Otherwise, False is returned.
 
         """
-        
         if self.value > card2.value:
             return True
         return False
@@ -86,10 +85,10 @@ class Card:
             The readable description of the card.
 
         """
-        
         card = '{} of {}'.format(self.rank, self.suit)
         return card
-        
+
+
 class Deck:
     """
     A card object.
@@ -111,7 +110,6 @@ class Deck:
         None.
 
         """
-        
         self.new_deck = []
         self.rank = ['Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
         self.suit = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
@@ -134,7 +132,6 @@ class Deck:
             Last card in deck is removed and returned.
 
         """
-        
         # If the deck is empty, no cards are returned.
         if len(self.new_deck) == 0:
             return
@@ -156,24 +153,7 @@ class Deck:
         trump = self.pick_card()
         
         return trump
-    
-    # def compare(self, card1, card2, trump):
 
-    #     if card1.suit == trump.suit and card2.suit != trump.suit:
-    #         winner = card1
-    #         return winner
-        
-    #     if card1.suit != trump.suit and card2.suit == trump.suit:
-    #         winner = card2
-    #         return winner
-
-    #     elif card1 > card2:
-    #         winner = card1
-    #         return winner
-
-    #     elif card1 < card2:
-    #         winner = card2
-    #         return winner
 
 class Player:
     """
@@ -191,7 +171,6 @@ class Player:
         * __init__(name)
 
     """
-    
     def __init__(self, name = None):
         
         # Each player object contains the player's name, the number of wins, 
@@ -200,7 +179,7 @@ class Player:
         self.name = name
         self.wins = 0
         self.hand = []
-        # self.card = None
+
 
 class Interface:
     """
@@ -209,6 +188,7 @@ class Interface:
     
     """
     pass
+
     
 class CmdLine(Interface):
     """
@@ -222,23 +202,50 @@ class CmdLine(Interface):
         * getvalid(trump_suit, list_of_cards, hand, position))
 
     """
-
     def getnames(self):
+        """
+        Have players enter their names in the command line.
+
+        Returns
+        -------
+        OBJ
+            Player 1 object.
+        OBJ
+            Player 2 object.
+
+        """
         self.p1 = input('Enter Player 1\'s Name: ')
         self.p2 = input('Enter Player 2\'s Name: ')
         
         return self.p1, self.p2
     
     def getcard(self, deck_obj):
+        """
+        Have player type card they wish to play in command line
+
+        Parameters
+        ----------
+        deck_obj : OBJ
+            Deck object.
+
+        Returns
+        -------
+        selected_card : STR
+            A string that describes the card that the player wants to play.
+
+        """
         selected_card = str(input('Which card would you like to play? '))
         
+        # The following steps are to check that the player has typed in a valid card
         check_string = selected_card.split(' ')
         
+        # The string must follow the form "{Rank} of {Suit}"
         while not len(check_string) == 3:
             print('Please type as seen on screen. Try again!')
             selected_card = str(input('Which card would you like to play? '))
             check_string = selected_card.split(' ')            
-
+        
+        # The rank and suit must be found in the deck object
         while not check_string[0] in deck_obj.rank and not check_string[2] in deck_obj.suit:
             print('Please check spelling and capitalization. Try again!')
             selected_card = str(input('Which card would you like to play? '))
@@ -247,11 +254,20 @@ class CmdLine(Interface):
         return selected_card
     
     def getmove(self):
-        
+        """
+        Have user decide whether to skip their turn (pass) or play a card
+
+        Returns
+        -------
+        STR
+            The move the player wants to make.
+
+        """
         selected_move = input('What would you like to do? Pass or Play? ')
         
         available_moves = ['pass', 'play']
         
+        # The spelling must be correct, but case does not matter
         while not selected_move.lower() in available_moves:
             print('Oops try typing \'pass\' or \'play\'')
             selected_move = input('What would you like to do? Pass or Play? ')
@@ -259,50 +275,143 @@ class CmdLine(Interface):
         return selected_move.lower()
     
     def getvalid(self, trump_suit, list_of_cards, hand, position):
-        
+        """
+        Find the cards that can be played
+
+        Parameters
+        ----------
+        trump_suit : STR
+            Suit of trump card object.
+        list_of_cards : LIST
+            Card objects on the table.
+        hand : LIST
+            Player's hand of cards. Stored in player object.
+        position : STR
+            Attacker or defender. The valid cards vary depending on position of the player.
+
+        Returns
+        -------
+        valid_cards : LIST
+            Card objects that can be played.
+
+        """
         valid_cards = []
         
+        # If the attacker has a chance to attack a second time, they can only play a card of
+        # the same rank as the last two cards on the table.
         if position == 'attacker':
             for c1 in list_of_cards[-2:]:
                 for c2 in hand:
                     if c1.rank == c2.rank:
                         valid_cards.append(c2)             
         
+        # The defender may play any card with a higher rank but same suit than the last card on the table or any
+        # card with the trump suit. If the card on the table has the trump suit, the player can only play cards 
+        # of the trump suit that have a higher rank.
         if position == 'defender':
             for c in hand:
                 if c.suit == list_of_cards[-1].suit and c > list_of_cards[-1]:
                     valid_cards.append(c)
-                if c.suit == trump_suit and c not in valid_cards:
+                if c.suit == trump_suit and list_of_cards[-1] != trump_suit:
                     valid_cards.append(c)
+                if c.suit == trump_suit and list_of_cards[-1].suit == trump_suit:
+                    if c > list_of_cards[-1]:
+                        valid_cards.append(c)
+            
+            # Any duplicates are removed from the valid cards list.
+            valid_cards = [*set(valid_cards)]
+        
+        # The list of valid cards is sorted by value
+        valid_cards.sort(key = lambda x: x.value)
+        
         return valid_cards
       
 class PyGame(Interface):
-    
+    """
+    Unfinished object to host the pygame interface. All functions are placeholders 
+    that will be the GUI corrollary to the command line version of the game.
+        
+    Functions:
+    --------
+        * __init__
+        * render_interface(window)
+        * getnames
+        * getcard
+        * getmove
+        * getvalid(trump_suit, list_of_cards, hand, position))
+
+    """    
     def __init__(self):
         pass
+    
     def render_interface(self, window):
-        window.fill((25, 130, 56))
+        """
+        Currently, this function displays a window where the game will take place when the GUI is created.
 
+        Parameters
+        ----------
+        window : OBJ
+            Pygame game window.
+
+        Returns
+        -------
+        None.
+
+        """
+        # Green background
+        window.fill((25, 130, 56))
+        
+        # Card design loaded
         cardBack = pygame.image.load('icons/design1.png')
         window.blit(cardBack, (300, 100))
         
+        # Text display
         font = pygame.font.SysFont('rockwell', 48, True)
         text = font.render('Work in progress!', True, (255, 255, 255))
         window.blit(text,(100, 500))
+        
+        # Update window
         pygame.display.update()
 
     def getnames(self):
         pass
+    
     def getcard(self):
         pass
+    
     def getmove(self):
         pass
+    
     def getvalid(self):
         pass
    
 class DurakGame:
+    """
+    The main game object.
+    
+    Functions:
+    --------
+        * setup
+        * wins(winner)
+        * deal(player, num = 1)
+        * play_a_card(player, card)
+        * play(interface, key = None, class_demo = False)
+        * run_game(interface)
 
+    """
     def setup(self):
+        """
+        
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+        trump_card : TYPE
+            DESCRIPTION.
+
+        """
+        
         self.deck = Deck()
         self.players = []
         
@@ -366,7 +475,7 @@ class DurakGame:
                 player.hand.remove(c)
                 return c
 
-    def play(self, interface, key = None, Demo = True):
+    def play(self, interface, key = None, class_demo = False):
         
         game_on = True
 
@@ -381,7 +490,9 @@ class DurakGame:
                 
                 # turn level
                 for turn in range(1, 100):
+                    
 
+                    
                     out_of_cards = False
                                 
                     if not out_of_cards:
@@ -394,25 +505,50 @@ class DurakGame:
                     for p in players:
                         p.hand.sort(key=lambda x: x.value)
 
-                    # if Demo:
-                    #     valid_response = False
+                    if class_demo:
+                        valid_response = False
 
-                    #     while not valid_response:
-                    #         stopping_point = input('Continue? ')
+                        while not valid_response:
+                            stopping_point = input('Continue? ')
 
-                    #         if stopping_point.lower() == 'yes':
-                    #             valid_response = True
-                    #         if stopping_point.lower() == 'end round':
-                    #             for c in players[0].hand:
-                    #                 print(c)
-                    #                 players[0].hand.remove(c)
-                    #             print('Length of {}\'s hand: {}'.format(players[0].name, len(players[0].hand)))
-                    #             valid_response = True
-                    #         if stopping_point.lower() == 'end game':
-                    #             players[0].wins = 5
-                    #             valid_response = True
+                            if stopping_point.lower() == 'yes':
+                                valid_response = True
+                            if stopping_point.lower() == 'end round':
+                                valid_response = True
+                            if stopping_point.lower() == 'end game':
+                                players[0].wins = 5
+                                valid_response = True
+                                
+                        if stopping_point == 'end round':
+                            for c in players[0].hand:
+                                    print(c)
+                                    players[0].hand = []
 
-                    end_turn = False
+                        if stopping_point == 'end game':
+                            players[0].hand = []
+                            players[0].wins = 4                            
+                    
+                    if len(players[0].hand) == 0:
+                        self.wins(players[0])
+                        sleep(2)
+                        print('{} wins this round!'.format(players[0].name))
+                        print('{} has {} wins.'.format(
+                            players[0].name, players[0].wins))
+                        print('{} has {} wins. First to 5 wins the game!'.format(
+                            players[1].name, players[1].wins))
+                        sleep(2)
+                        break
+
+                    elif len(players[1].hand) == 0:
+                        self.wins(players[1])
+                        sleep(2)
+                        print('{} wins this round!'.format(players[1].name))
+                        print('{} has {} wins.'.format(
+                            players[1].name, players[1].wins))
+                        print('{} has {} wins. First to 5 wins the game!'.format(
+                            players[0].name, players[0].wins))
+                        sleep(2)
+                        break
                     
                     print('~~~~~~~~~')
                     print('TURN %i' % turn)
@@ -443,7 +579,9 @@ class DurakGame:
                     print('\nThe defender may accept the attack, ending their turn,')
                     print('or they may defend with a better card if they have one.\n')
                     sleep(2)
-
+                    
+                    end_turn = False
+                    
                     while not end_turn:
                         # getting move from defender
                         current_player = defender
@@ -451,7 +589,9 @@ class DurakGame:
                                 trump_card.suit, battle_cards, defender.hand, 'defender')
                         
                         if len(valid_cards) == 0:
-                                print('No valid card to play. Must take the attack.')
+                                print('No valid card to play.') 
+                                print('{} must take the attack and add cards to their hand.'.format(defender.name))
+                                
                                 sleep(3)
                                 for c in battle_cards:
                                     defender.hand.append(c)
@@ -488,7 +628,7 @@ class DurakGame:
 
                         current_player = attacker
 
-                        print('\nThe attacker {} may choose to pass or attack again.'.format(attacker.name))
+                        print('\nThe attacker may choose to pass or attack again.')
                         print('The attacker is {}. {}, look away!'.format(attacker.name, defender.name))
                         sleep(5)
                         print('Cards in {}\'s hand: {}\n'.format(attacker.name, attacker.hand))
@@ -497,7 +637,7 @@ class DurakGame:
                                 trump_card.suit, battle_cards, attacker.hand, 'attacker')
 
                         if len(valid_cards) == 0:
-                                print('No valid card to play. Must pass.')
+                                print('No valid card to play. Must pass and discard cards.')
                                 sleep(3)
                                 for c in battle_cards:
                                     battle_cards.pop()
@@ -505,7 +645,7 @@ class DurakGame:
                                 end_turn = True
                                 continue
 
-                        print('You can play any of these cards from your hand: {}'.format(valid_cards))
+                        print('{}, you can play any of these cards from your hand: {}'.format(attacker.name, valid_cards))
                         print('Trump suit: {}'.format(trump_card.suit))
                         a_move = interface.getmove()
                         
@@ -529,38 +669,15 @@ class DurakGame:
                             print('\n{} has attacked with {}'.format(attacker.name, attacker_card))
                             print('__________________________________________')
 
-                    if len(players[0].hand) == 0:
-                        self.wins(players[0])
-                        sleep(2)
-                        print('{} wins this round!'.format(players[0].name))
-                        print('{} has {} wins.'.format(
-                            players[0].name, players[0].wins))
-                        print('{} has {} wins. First to 5 wins the game!'.format(
-                            players[1].name, players[1].wins))
-                        sleep(2)
-                        break
-
-                    elif len(players[1].hand) == 0:
-                        self.wins(players[1])
-                        sleep(2)
-                        print('{} wins this round!'.format(players[1].name))
-                        print('{} has {} wins.'.format(
-                            players[1].name, players[1].wins))
-                        print('{} has {} wins. First to 5 wins the game!'.format(
-                            players[0].name, players[0].wins))
-                        sleep(2)
-                        break
-
-
             if players[0].wins == 5:
                 print('***********************')
-                print('\n{} wins the game!'.format(players[0].name))
-                print('***********************')
+                print('\n{} wins!'.format(players[0].name))
+                print('\n***********************')
                 game_on = False
             if players[1].wins == 5:
                 print('***********************')
-                print('\n{} wins the game!'.format(players[1].name))
-                print('***********************')
+                print('\n{} wins!'.format(players[1].name))
+                print('\n***********************')
                 game_on = False
 
     def run_game(self, interface):
@@ -589,4 +706,4 @@ if __name__ == "__main__":
         if intObj == PyGame():
             gamelogic.run_game(intObj)
         else:
-            gamelogic.play(intObj)
+            gamelogic.play(intObj, class_demo = True)
